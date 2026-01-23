143e9ffc1eb8137edb2c35f89a952a9f415a8db6
LuckVidal07 <lucasvidalbr07@gmail.com>
2026-01-23 11:29:23 -0300

    refactor: move Logicore.php to bin directory

--- bin/Logicore.php
+++ bin/Logicore.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Infrastructure\JsonProdutoRepository;
 use App\Service\LogService;
